00b0462479c943a8784f097b000aee726a21a76b
Elias Sauer <jels1991@gmail.com>
2017-03-14 12:20:44 -0400

    Cambios en el master (elias)
    cambie el nombre de la pagina

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Elias
                 </div>
 
                 <div class="links">
